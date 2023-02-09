56613345b13339ac5d7d6826da2d2e5a568a8469
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-09 11:14:28 -0600

    Made hypotenuse deadband configurable.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swervedrive2/swervelib/parser/SwerveControllerConfiguration.java
+++ src/main/java/frc/robot/subsystems/swervedrive2/swervelib/parser/SwerveControllerConfiguration.java
@@ -20,21 +20,35 @@ public class SwerveControllerConfiguration
   /**
    * hypotenuse deadband for the robot angle control joystick.
    */
-  public       double     hypotDeadband = 0.5; // Deadband for the minimum hypot for the heading joystick.
+  public final double     hypotDeadband; // Deadband for the minimum hypot for the heading joystick.
 
   /**
    * Construct the swerve controller configuration.
    *
-   * @param driveCfg    Drive configuration.
-   * @param headingPIDF Heading PIDF configuration.
+   * @param driveCfg      Drive configuration.
+   * @param headingPIDF   Heading PIDF configuration.
+   * @param hypotDeadband Deadband on radius of angle joystick.
    */
-  public SwerveControllerConfiguration(SwerveDriveConfiguration driveCfg, PIDFConfig headingPIDF)
+  public SwerveControllerConfiguration(SwerveDriveConfiguration driveCfg, PIDFConfig headingPIDF, double hypotDeadband)
   {
     this.maxSpeed = driveCfg.maxSpeed;
     this.maxAngularVelocity = calculateMaxAngularVelocity(driveCfg.maxSpeed,
                                                           driveCfg.moduleLocationsMeters[0].getX(),
                                                           driveCfg.moduleLocationsMeters[0].getY());
     this.headingPIDF = headingPIDF;
+    this.hypotDeadband = hypotDeadband;
+  }
+
+  /**
+   * Construct the swerve controller configuration. Assumes hypotenuse deadband of 0.5 (minimum radius for angle to be
+   * set on angle joystick is .5 of the controller).
+   *
+   * @param driveCfg    Drive configuration.
+   * @param headingPIDF Heading PIDF configuration.
+   */
+  public SwerveControllerConfiguration(SwerveDriveConfiguration driveCfg, PIDFConfig headingPIDF)
+  {
+    this(driveCfg, headingPIDF, 0.5);
   }
 
 }

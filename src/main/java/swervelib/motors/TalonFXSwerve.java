7475163b375f1263b51441b8283bc8d446e83cd6
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-13 16:57:38 -0600

    Seperated velocity motor from angle motor.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/swervelib/motors/TalonFXSwerve.java
+++ src/main/java/swervelib/motors/TalonFXSwerve.java
@@ -213,7 +213,7 @@ public class TalonFXSwerve extends SwerveMotor
   @Override
   public double getVelocity()
   {
-    return motor.getSelectedSensorVelocity() * 100;
+    return isDriveMotor ? motor.getSelectedSensorVelocity() * 100 : motor.getSelectedSensorVelocity();
   }
 
   /**

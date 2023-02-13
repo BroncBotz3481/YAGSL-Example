00ea232a98db2355af8d012a18745aa744e38387
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-13 16:56:18 -0600

    Changed meters per 100ms to meters per second.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/swervelib/motors/TalonFXSwerve.java
+++ src/main/java/swervelib/motors/TalonFXSwerve.java
@@ -213,7 +213,7 @@ public class TalonFXSwerve extends SwerveMotor
   @Override
   public double getVelocity()
   {
-    return motor.getSelectedSensorVelocity();
+    return motor.getSelectedSensorVelocity() * 100;
   }
 
   /**

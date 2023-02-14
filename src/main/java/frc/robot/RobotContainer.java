7202f420a10c132906293e624a6e959e32d00e67
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-13 22:32:30 -0600

    Simple path planner autonomous.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -89,7 +89,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return Autos.driveAndSpin(drivebase);
+    return Autos.exampleAuto(drivebase);
   }
 
   public void setDriveMode()
--- src/main/java/frc/robot/commands/swervedrive2/auto/Autos.java
+++ src/main/java/frc/robot/commands/swervedrive2/auto/Autos.java
@@ -7,12 +7,13 @@ package frc.robot.commands.swervedrive2.auto;
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
-import frc.robot.Constants.Auton;
 import frc.robot.subsystems.swervedrive2.SwerveSubsystem;
 
 public final class Autos
@@ -28,8 +29,18 @@ public final class Autos
    */
   public static CommandBase exampleAuto(SwerveSubsystem swerve)
   {
-    PathPlannerTrajectory example = PathPlanner.loadPath("New Path",
-                                                         new PathConstraints(Auton.MAX_SPEED, Auton.MAX_ACCELERATION));
+//    PathPlannerTrajectory example = PathPlanner.loadPath("New Path",
+//                                                         new PathConstraints(Auton.MAX_SPEED, Auton.MAX_ACCELERATION));
+    // Simple path with holonomic rotation. Stationary start/end. Max velocity of 4 m/s and max accel of 3 m/s^2
+    PathPlannerTrajectory example = PathPlanner.generatePath(
+        new PathConstraints(4, 3),
+        new PathPoint(new Translation2d(0, 0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+// position, heading(direction of travel), holonomic rotation
+        new PathPoint(new Translation2d(1.0, 1.0), Rotation2d.fromDegrees(0), Rotation2d.fromDegrees(0)),
+// position, heading(direction of travel), holonomic rotation
+        new PathPoint(new Translation2d(3.0, 3.0), Rotation2d.fromDegrees(45), Rotation2d.fromDegrees(-90))
+        // position, heading(direction of travel), holonomic rotation
+                                                            );
     return Commands.sequence(new FollowTrajectory(swerve, example, true));
   }
 

f5e8d94f30825658e1c91accdd2d9c8e242c47ef
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-02-14 20:40:48 -0500

    import the library

--- src/main/java/swervelib/SwerveDrive.java
+++ src/main/java/swervelib/SwerveDrive.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;

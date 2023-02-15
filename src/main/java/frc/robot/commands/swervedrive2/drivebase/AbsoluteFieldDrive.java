b982ad7ee539d0cec8f5b5da746080ff04009ad6
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-15 13:22:23 -0600

    Fixed AbsoluteFieldDrive, moved SwerveModule simulation details into SwerveModuleSimulation.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/commands/swervedrive2/drivebase/AbsoluteFieldDrive.java
+++ src/main/java/frc/robot/commands/swervedrive2/drivebase/AbsoluteFieldDrive.java
@@ -64,7 +64,7 @@ public class AbsoluteFieldDrive extends CommandBase
     // Get the desired chassis speeds based on a 2 joystick module.
 
     ChassisSpeeds desiredSpeeds = swerve.getTargetSpeeds(vX.getAsDouble(), vY.getAsDouble(),
-                                                         new Rotation2d(heading.getAsDouble()));
+                                                         new Rotation2d(heading.getAsDouble() * Math.PI));
 
     // Limit velocity to prevent tippy
     Translation2d translation = SwerveController.getTranslation2d(desiredSpeeds);

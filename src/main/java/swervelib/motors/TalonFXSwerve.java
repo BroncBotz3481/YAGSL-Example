380e601223fa556d4fdebe856680ee0690da5c63
nt <89554543+7910f6ba7ee4@users.noreply.github.com>
2023-02-16 20:15:15 -0500

    remove duplicate 0360 function

--- src/main/java/swervelib/motors/TalonFXSwerve.java
+++ src/main/java/swervelib/motors/TalonFXSwerve.java
@@ -87,50 +87,6 @@ public class TalonFXSwerve extends SwerveMotor
   }
 
   /**
-   * Put an angle within the the 360 deg scope of a reference. For example, given a scope reference
-   * of 756 degrees, assumes the full scope is (720-1080), and places an angle of 22 degrees into
-   * it, returning 742 deg.
-   *
-   * @param scopeReference Current Angle (deg)
-   * @param newAngle       Target Angle (deg)
-   * @return               Closest angle within scope (deg)
-   */
-  private double placeInAppropriate0To360Scope(double scopeReference, double newAngle) {
-    double lowerBound;
-    double upperBound;
-    double lowerOffset = scopeReference % 360;
-
-    // Create the interval from the reference angle. 
-    if (lowerOffset >= 0) 
-    {
-      lowerBound = scopeReference - lowerOffset;
-      upperBound = scopeReference + (360 - lowerOffset);
-    } else 
-    {
-      upperBound = scopeReference - lowerOffset;
-      lowerBound = scopeReference - (360 + lowerOffset);
-    }
-    // Put the angle in the interval.
-    while (newAngle < lowerBound) 
-    {
-      newAngle += 360;
-    }
-    while (newAngle > upperBound) 
-    {
-      newAngle -= 360;
-    }
-    // Smooth the transition between interval boundaries.
-    if (newAngle - scopeReference > 180) 
-    {
-      newAngle -= 360;
-    } else if (newAngle - scopeReference < -180) 
-    {
-      newAngle += 360;
-    }
-    return newAngle;
-  }
-
-  /**
    * Configure the factory defaults.
    */
   @Override

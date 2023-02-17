b341c571c3f30437265dd966a4c39c3e6724548e
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-02-16 23:16:38 -0600

    Added example pathplanner auto which uses stop events.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/commands/swervedrive2/auto/Autos.java
+++ src/main/java/frc/robot/commands/swervedrive2/auto/Autos.java
@@ -8,13 +8,20 @@ import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import frc.robot.Constants.Auton;
 import frc.robot.subsystems.swervedrive2.SwerveSubsystem;
+import java.util.HashMap;
+import java.util.List;
 
 public final class Autos
 {
@@ -45,7 +52,31 @@ public final class Autos
                                         );
     } else
     {
-      example = PathPlanner.loadPath("SamplePath", new PathConstraints(4, 3));
+      List<PathPlannerTrajectory> example1 = PathPlanner.loadPathGroup("SamplePath", new PathConstraints(4, 3));
+      // This is just an example event map. It would be better to have a constant, global event map
+      // in your code that will be used by all path following commands.
+      HashMap<String, Command> eventMap = new HashMap<>();
+      eventMap.put("marker1", new PrintCommand("Passed marker 1"));
+
+      // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
+      SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+          swerve::getPose,
+// Pose2d supplier
+          swerve::resetOdometry,
+// Pose2d consumer, used to reset odometry at the beginning of auto
+          new PIDConstants(Auton.yAutoPID.p, Auton.yAutoPID.i, Auton.yAutoPID.d),
+// PID constants to correct for translation error (used to create the X and Y PID controllers)
+          new PIDConstants(Auton.angleAutoPID.p, Auton.angleAutoPID.i, Auton.angleAutoPID.d),
+// PID constants to correct for rotation error (used to create the rotation controller)
+          swerve::setChassisSpeeds,
+// Module states consumer used to output to the drive subsystem
+          eventMap,
+          false,
+// Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+          swerve
+// The drive subsystem. Used to properly set the requirements of path following commands
+      );
+      return Commands.sequence(autoBuilder.fullAuto(example1));
     }
 //    swerve.postTrajectory(example);
     return Commands.sequence(new FollowTrajectory(swerve, example, true));

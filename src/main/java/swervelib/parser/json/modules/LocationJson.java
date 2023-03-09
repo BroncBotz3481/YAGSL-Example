9023f5ca0197a490f12af4e028a7566ce1d907f9
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-03-09 15:44:57 -0600

    Added backwards compatibility.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/swervelib/parser/json/modules/LocationJson.java
+++ src/main/java/swervelib/parser/json/modules/LocationJson.java
@@ -10,9 +10,9 @@ public class LocationJson
   /**
    * Location of the swerve module in inches from the center of the robot horizontally.
    */
-  public double front;
+  public double front = 0, x = 0;
   /**
    * Location of the swerve module in inches from the center of the robot vertically.
    */
-  public double left;
+  public double left = 0, y = 0;
 }

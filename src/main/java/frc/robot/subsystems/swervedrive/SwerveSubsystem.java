e9fc0036e6c00282e60f983329ec330e64d93929
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2023-03-22 15:34:01 -0500

    Added factory function to the subsystem that shows how to generate a path planner command easily.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -4,14 +4,22 @@
 
 package frc.robot.subsystems.swervedrive;
 
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.io.File;
+import java.util.List;
+import java.util.Map;
 import swervelib.SwerveController;
 import swervelib.SwerveDrive;
 import swervelib.math.SwerveKinematics2;
@@ -27,7 +35,12 @@ public class SwerveSubsystem extends SubsystemBase
   /**
    * Swerve drive object.
    */
-  private final SwerveDrive swerveDrive;
+  private final SwerveDrive       swerveDrive;
+  /**
+   * The auto builder for PathPlanner, there can only ever be one created so we save it just incase we generate multiple
+   * paths with events.
+   */
+  private       SwerveAutoBuilder autoBuilder = null;
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
@@ -265,5 +278,47 @@ public class SwerveSubsystem extends SubsystemBase
   public void addFakeVisionReading()
   {
     swerveDrive.addVisionMeasurement(new Pose2d(3, 3, Rotation2d.fromDegrees(65)), Timer.getFPGATimestamp(), false, 1);
+  }
+
+  /**
+   * Factory to fetch the PathPlanner command to follow the defined path.
+   *
+   * @param path             Path planner path to specify.
+   * @param constraints      {@link PathConstraints} for {@link com.pathplanner.lib.PathPlanner#loadPathGroup} function
+   *                         limiting velocity and acceleration.
+   * @param eventMap         {@link java.util.HashMap} of commands corresponding to path planner events given as
+   *                         strings.
+   * @param translation      The {@link PIDConstants} for the translation of the robot while following the path.
+   * @param rotation         The {@link PIDConstants} for the rotation of the robot while following the path.
+   * @param useAllianceColor Automatically transform the path based on alliance color.
+   * @return PathPlanner command to follow the given path.
+   */
+  public Command pathPlannerCommand(String path, PathConstraints constraints, Map<String, Command> eventMap,
+                                    PIDConstants translation, PIDConstants rotation, boolean useAllianceColor)
+  {
+    List<PathPlannerTrajectory> pathGroup = PathPlanner.loadPathGroup(path, constraints);
+//    SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+//      Pose2d supplier,
+//      Pose2d consumer- used to reset odometry at the beginning of auto,
+//      PID constants to correct for translation error (used to create the X and Y PID controllers),
+//      PID constants to correct for rotation error (used to create the rotation controller),
+//      Module states consumer used to output to the drive subsystem,
+//      Should the path be automatically mirrored depending on alliance color. Optional- defaults to true
+//   )
+    if (autoBuilder == null)
+    {
+      autoBuilder = new SwerveAutoBuilder(
+          swerveDrive::getPose,
+          swerveDrive::resetOdometry,
+          translation,
+          rotation,
+          swerveDrive::setChassisSpeeds,
+          eventMap,
+          useAllianceColor,
+          this
+      );
+    }
+
+    return autoBuilder.fullAuto(pathGroup);
   }
 }

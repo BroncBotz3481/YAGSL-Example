37aa07dba547685dad3f364ea485d6f102d1b4cb
Roboducks 1561 <81271101+MrXpira@users.noreply.github.com>
2023-03-23 23:37:21 -0700

    Arm and shooter functionality
    Added two buttons to reset arm and set arm brake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,24 +5,33 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RepeatCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.AutoMap;
-import frc.robot.commands.swervedrive.auto.GoToPose;
 import frc.robot.commands.swervedrive.auto.GoToScoring;
 import frc.robot.commands.swervedrive.auto.PathBuilder;
 import frc.robot.commands.swervedrive.auto.GoToScoring.POSITION;
 import frc.robot.commands.swervedrive.drivebase.TeleopDrive;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Arm.ArmPosition;
+import frc.robot.subsystems.Shooter.ShootSpeed;
 import frc.robot.subsystems.swervedrive.SwerveSubsystem;
 
 import java.io.File;
+
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
@@ -39,7 +48,8 @@ public class RobotContainer
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
                                                                          "swerve/falcon"));
-  //private final Shooter shooter = new Shooter();
+  private final Shooter shooter = new Shooter();
+  private final Arm arm = new Arm();
   // CommandJoystick rotationController = new CommandJoystick(1);
   // Replace with CommandPS4Controller or CommandJoystick if needed
   //CommandJoystick driverController = new CommandJoystick(1);
@@ -55,6 +65,8 @@ public class RobotContainer
   private final int translationAxis = XboxController.Axis.kLeftY.value;
   private final int strafeAxis = XboxController.Axis.kLeftX.value;
   private final int rotationAxis = XboxController.Axis.kRightX.value;
+  private final DigitalInput resetArmSwitch = new DigitalInput(0); // Limit switch on DIO 3
+  private final DigitalInput unlockArmSwitch = new DigitalInput(1); // Limit switch on DIO 3
 
 
   private final TeleopDrive closedFieldRel =
@@ -119,11 +131,20 @@ public class RobotContainer
    */
   private void configureBindings()
   {
+    Trigger resetArm = new Trigger(resetArmSwitch::get);
+    Trigger unlockArm = new Trigger(unlockArmSwitch::get);
+    if (DriverStation.isDisabled()) {
+      resetArm.onTrue(arm.resetArm());
+      unlockArm.onTrue(new InstantCommand(() -> arm.setBrake(false))).onFalse(new InstantCommand(() -> arm.setBrake(true)));  
+    }
+    
     driverXbox.a().whileTrue(new RepeatCommand(new InstantCommand(drivebase::lock, drivebase)));
     driverXbox.y().onTrue((new InstantCommand(drivebase::zeroGyro)));
 
-
-
+    operatorXbox.a().whileTrue(new SequentialCommandGroup(arm.moveArmToPosition(ArmPosition.Low), shooter.shoot(ShootSpeed.Low)));
+    operatorXbox.b().whileTrue(new SequentialCommandGroup(arm.moveArmToPosition(ArmPosition.Mid), shooter.shoot(ShootSpeed.Mid)));
+    operatorXbox.y().whileTrue(new SequentialCommandGroup(arm.moveArmToPosition(ArmPosition.High), shooter.shoot(ShootSpeed.High)));
+    operatorXbox.x().whileTrue(new SequentialCommandGroup(arm.moveArmToPosition(ArmPosition.Intake), shooter.intake()));
     operatorXbox.povRight().whileTrue(new GoToScoring(drivebase, POSITION.RIGHT).getCommand());
     operatorXbox.povDown().whileTrue(new GoToScoring(drivebase, POSITION.MIDDLE).getCommand());
     operatorXbox.povLeft().whileTrue(new GoToScoring(drivebase, POSITION.LEFT).getCommand());

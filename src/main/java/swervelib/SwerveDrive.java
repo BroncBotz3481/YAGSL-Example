c2604360e4f1e379fbfaa09351685783e00ce887
T Grinch <10247070+thenetworkgrinch@users.noreply.github.com>
2023-11-29 16:53:57 -0600

    Included new telemetry for encoder reading failures based of TechnologyMan's PR #27 in YAGSL.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/swervelib/SwerveDrive.java
+++ src/main/java/swervelib/SwerveDrive.java
@@ -901,6 +901,8 @@ public class SwerveDrive
               "Module[" + module.configuration.name + "] Relative Encoder", module.getRelativePosition());
           SmartDashboard.putNumber(
               "Module[" + module.configuration.name + "] Absolute Encoder", module.getAbsolutePosition());
+          SmartDashboard.putNumber(
+                "Module[" + module.configuration.name + "] Absolute Encoder Read Issue", module.getAbsoluteEncoderReadIssue() ? 1 : 0);
         }
         if (SwerveDriveTelemetry.verbosity.ordinal() >= TelemetryVerbosity.HIGH.ordinal())
         {

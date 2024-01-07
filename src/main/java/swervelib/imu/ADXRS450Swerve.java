8ba80e1ba17669ba5c2be8259022a6261ae3884a
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2024-01-06 23:08:49 -0600

    Added back ADXRS

---
+++ src/main/java/swervelib/imu/ADXRS450Swerve.java
@@ -0,0 +1,105 @@
+package swervelib.imu;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.Optional;
+
+/**
+ * IMU Swerve class for the {@link ADXRS450_Gyro} device.
+ */
+public class ADXRS450Swerve extends SwerveIMU
+{
+
+  /**
+   * {@link ADXRS450_Gyro} device to read the current headings from.
+   */
+  private final ADXRS450_Gyro imu;
+  /**
+   * Offset for the ADXRS450.
+   */
+  private       Rotation3d    offset = new Rotation3d();
+
+  /**
+   * Construct the ADXRS450 imu and reset default configurations. Publish the gyro to the SmartDashboard.
+   */
+  public ADXRS450Swerve()
+  {
+    imu = new ADXRS450_Gyro();
+    factoryDefault();
+    SmartDashboard.putData(imu);
+  }
+
+  /**
+   * Reset IMU to factory default.
+   */
+  @Override
+  public void factoryDefault()
+  {
+    offset = new Rotation3d(0, 0, Math.toRadians(imu.getAngle()));
+  }
+
+  /**
+   * Clear sticky faults on IMU.
+   */
+  @Override
+  public void clearStickyFaults()
+  {
+    // Do nothing.
+  }
+
+  /**
+   * Set the gyro offset.
+   *
+   * @param offset gyro offset as a {@link Rotation3d}.
+   */
+  public void setOffset(Rotation3d offset)
+  {
+    this.offset = offset;
+  }
+
+  /**
+   * Fetch the {@link Rotation3d} from the IMU without any zeroing. Robot relative.
+   *
+   * @return {@link Rotation3d} from the IMU.
+   */
+  public Rotation3d getRawRotation3d()
+  {
+    return new Rotation3d(0, 0, Math.toRadians(imu.getAngle()));
+  }
+
+  /**
+   * Fetch the {@link Rotation3d} from the IMU. Robot relative.
+   *
+   * @return {@link Rotation3d} from the IMU.
+   */
+  @Override
+  public Rotation3d getRotation3d()
+  {
+    return getRawRotation3d().minus(offset);
+  }
+
+  /**
+   * Fetch the acceleration [x, y, z] from the IMU in meters per second squared. If acceleration isn't supported returns
+   * empty.
+   *
+   * @return {@link Translation3d} of the acceleration as an {@link Optional}.
+   */
+  @Override
+  public Optional<Translation3d> getAccel()
+  {
+    return Optional.empty();
+  }
+
+  /**
+   * Get the instantiated IMU object.
+   *
+   * @return IMU object.
+   */
+  @Override
+  public Object getIMU()
+  {
+    return imu;
+  }
+}

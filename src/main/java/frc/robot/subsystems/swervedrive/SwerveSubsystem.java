87b496cd683d52120e5870eba4e3181901544181
Marshall Tappen <mtappen@gmail.com>
2024-01-09 12:52:51 -0800

    Updated followPathWithEvents from this deprecated name to the proper name of followPath.

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -136,7 +136,7 @@ public class SwerveSubsystem extends SubsystemBase
     }
 
     // Create a path following command using AutoBuilder. This will also trigger event markers.
-    return AutoBuilder.followPathWithEvents(path);
+    return AutoBuilder.followPath(path);
   }
 
   /**

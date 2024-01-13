31d94cdbb400ae0c403eed03b2f01824a7c3740e
DriverStation3 <programming@robototes.com>
2024-01-13 12:33:31 -0800

    fix unit error with cosine compensator

--- src/main/java/swervelib/SwerveModule.java
+++ src/main/java/swervelib/SwerveModule.java
@@ -189,7 +189,7 @@ public class SwerveModule
       double steerMotorError = desiredState.angle.getDegrees() - getAbsolutePosition();
       /* If error is close to 0 rotations, we're already there, so apply full power */
       /* If the error is close to 0.25 rotations, then we're 90 degrees, so movement doesn't help us at all */
-      double cosineScalar = Math.cos(Units.rotationsToRadians(steerMotorError));
+      double cosineScalar = Math.cos(Units.degreesToRadians(steerMotorError));
       /* Make sure we don't invert our drive, even though we shouldn't ever target over 90 degrees anyway */
       if (cosineScalar < 0.0)
       {

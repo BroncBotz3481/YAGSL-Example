e11bba8fe25d0ce469dfa05de85d33b87027244a
T Grinch <10247070+thenetworkgrinch@users.noreply.github.com>
2024-02-06 16:38:26 -0600

    Added photonlib json. Changed PathPlannerAuto.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -120,7 +120,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Path", true);
+    return drivebase.getAutonomousCommand("New Auto");
   }
 
   public void setDriveMode()

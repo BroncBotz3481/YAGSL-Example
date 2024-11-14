a6ea5194867dfd6ffd1679551e81ab6342a15d52
Brayden N <gamerinfusion@gmail.com>
2024-11-13 20:09:19 -0500

    SWERVE WORKS

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,7 @@ public class RobotContainer
   final CommandXboxController driverXbox = new CommandXboxController(0);
   // The robot's subsystems and commands are defined here...
   private final SwerveSubsystem drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
-                                                                         "swerve/neo"));
+                                                                         "swerve/pinchy"));
   // Applies deadbands and inverts controls because joysticks
   // are back-right positive while robot
   // controls are front-left positive

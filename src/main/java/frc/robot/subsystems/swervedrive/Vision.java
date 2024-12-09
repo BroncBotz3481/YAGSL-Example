5e0a89d30477d69f603f9a5c9df43aede7d02efa
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2024-12-09 08:45:07 -0600

    Fixed vision null ptr

--- src/main/java/frc/robot/subsystems/swervedrive/Vision.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Vision.java
@@ -402,7 +402,7 @@ public class Vision
     /**
      * Results list to be updated periodically and cached to avoid unnecessary queries.
      */
-    public        List<PhotonPipelineResult>   resultsList;
+    public        List<PhotonPipelineResult>   resultsList = new ArrayList<>();
     /**
      * Last read from the camera timestamp to prevent lag due to slow data fetches.
      */
@@ -522,7 +522,7 @@ public class Vision
      */
     private void updateUnreadResults()
     {
-      double mostRecentTimestamp = resultsList.get(0).getTimestampSeconds();
+      double mostRecentTimestamp = resultsList.isEmpty() ? 0.0 : resultsList.get(0).getTimestampSeconds();
       double currentTimestamp    = Microseconds.of(NetworkTablesJNI.now()).in(Seconds);
       double debounceTime        = Milliseconds.of(15).in(Seconds);
       for (PhotonPipelineResult result : resultsList)

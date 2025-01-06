b224eafe2e783e002e28b902d96c0b03556747bf
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2025-01-06 12:33:59 -0600

    Added srxmag standalone.
    Signed-off-by: thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>

--- src/main/java/swervelib/parser/json/DeviceJson.java
+++ src/main/java/swervelib/parser/json/DeviceJson.java
@@ -96,6 +96,9 @@ public class DeviceJson
         return new AnalogAbsoluteEncoderSwerve(id);
       case "cancoder":
         return new CANCoderSwerve(id, canbus != null ? canbus : "");
+      case "srxmag_standalone":
+        return new TalonSRXEncoderSwerve(new TalonSRXSwerve(id, false, DCMotor.getCIM(1)),
+                                         FeedbackDevice.PulseWidthEncodedPosition);
       case "talonsrx_pwm":
         return new TalonSRXEncoderSwerve(motor, FeedbackDevice.PulseWidthEncodedPosition);
       case "talonsrx_analog":

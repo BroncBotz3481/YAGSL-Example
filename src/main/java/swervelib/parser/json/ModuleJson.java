c581566386dc11b33270d1dfed2f586e1f11430a
Wackyvert <wvinc.com@gmail.com>
2025-01-22 21:29:54 -0600

    Use correct values

--- src/main/java/swervelib/parser/json/ModuleJson.java
+++ src/main/java/swervelib/parser/json/ModuleJson.java
@@ -76,7 +76,7 @@ public class ModuleJson
     SwerveAbsoluteEncoder absEncoder = encoder.createEncoder(angleMotor);
 
     //Throw an error if module locations are improperly set
-    if (location.x==0 && location.y==0)
+    if (location.front==0 && location.left==0)
     {
       throw new RuntimeException("Improper Module Location Settings!\n" +
               "Your module location is set to 0 for both 'front' and 'left' values.\n" +
@@ -127,8 +127,8 @@ public class ModuleJson
         conversionFactors,
         absEncoder,
         absoluteEncoderOffset,
-        Units.inchesToMeters(Math.round(location.x)),
-        Units.inchesToMeters(Math.round(location.y)),
+        Units.inchesToMeters(Math.round(location.front)),
+        Units.inchesToMeters(Math.round(location.left)),
         anglePIDF,
         velocityPIDF,
         physicalCharacteristics,

70ebdc0cab9be8cb90dc6233c03e312cf4033f4a
Kaya <95276965+kytpbs@users.noreply.github.com>
2025-02-05 14:56:54 +0300

    Remove aprilTagFieldLayout from SwerveSubsystem
    It already exists in `Vision.java` and is not used in any commented-out code either

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -64,10 +64,6 @@ public class SwerveSubsystem extends SubsystemBase
    */
   private final SwerveDrive         swerveDrive;
   /**
-   * AprilTag field layout.
-   */
-  private final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2024Crescendo);
-  /**
    * Enable vision odometry updates while driving.
    */
   private final boolean             visionDriveTest     = false;

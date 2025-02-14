0e78d3615836bfa60e1747db6cfe6647101f8199
Max LePrell <maximusleprell@gmail.com>
2025-02-14 15:42:11 -0500

    inital configurarion and fixes to rid of starrt up errors.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -163,7 +163,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Auto");
+    return drivebase.getAutonomousCommand("SmallTestAuto");
   }
 
   public void setMotorBrake(boolean brake)

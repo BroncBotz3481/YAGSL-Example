4c99bf534228c787d16b06febaaab797418673be
T Grinch <10247070+thenetworkgrinch@users.noreply.github.com>
2025-03-09 09:55:13 -0500

    Update SparkMaxSwerve.java

--- src/main/java/swervelib/motors/SparkMaxSwerve.java
+++ src/main/java/swervelib/motors/SparkMaxSwerve.java
@@ -391,10 +391,6 @@ public class SparkMaxSwerve extends SwerveMotor
   @Override
   public void burnFlash()
   {
-    if (!DriverStation.isDisabled())
-    {
-      throw new RuntimeException("Config updates cannot be applied while the robot is Enabled!");
-    }
     configureSparkMax(() -> {
       return motor.configure(cfg, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);
     });
